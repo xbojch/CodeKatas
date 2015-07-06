c6898cf2caeb73c60e9b380537d329d739c4d51e
Bojan Bedrac <bojan@bele-serv.com>
2015-07-06 21:28:46 +0000

    Use a switch statement in UpdateQuality method

--- classes/GildedRose/Program.php
+++ classes/GildedRose/Program.php
@@ -60,54 +60,75 @@ class Program
 
     public function UpdateQuality()
     {
-        for ($i = 0; $i < count($this->items); $i++) {
-            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                if ($this->items[$i]->quality > 0) {
-                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                        $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                    }
-                }
-            } else {
-                if ($this->items[$i]->quality < 50) {
-                    $this->items[$i]->quality = $this->items[$i]->quality + 1;
+        foreach ($this->items as $item) {
 
-                    if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->sellIn < 11) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
-                        }
+            switch($item->name) {
+                case 'Aged Brie':
+                    $this->brie($item);
+                    break;
 
-                        if ($this->items[$i]->sellIn < 6) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
-                        }
-                    }
-                }
+                case 'Sulfuras, Hand of Ragnaros':
+                    $this->sulfuras($item);
+                    break;
+
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->backstage($item);
+                    break;
+
+                default:
+                    $this->normal($item);
+                    break;
             }
 
-            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                $this->items[$i]->sellIn = $this->items[$i]->sellIn - 1;
-            }
-
-            if ($this->items[$i]->sellIn < 0) {
-                if ($this->items[$i]->name != "Aged Brie") {
-                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->quality > 0) {
-                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                                $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                            }
-                        }
-                    } else {
-                        $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
-                    }
-                } else {
-                    if ($this->items[$i]->quality < 50) {
-                        $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                    }
-                }
-            }
         }
+    }
+
+    private function normal($item)
+    {
+        $item->sellIn -= 1;
+
+        if ($item->quality == 0) return $item;
+
+        $item->quality -= 1;
+
+        if ($item->sellIn <=0) $item->quality -= 1;
+
+        return $item;
+    }
+
+    private function brie($item)
+    {
+        $item->sellIn -= 1;
+
+        if ($item->quality >= 50) return $item;
+
+        $item->quality += 1;
+
+        if ($item->sellIn < 0) $item->quality += 1;
+
+        return $item;
+    }
+
+    private function sulfuras($item)
+    {
+        return $item;
+    }
+
+    private function backstage($item)
+    {
+        $item->sellIn -= 1;
+
+        if ($item->quality >= 50) return $item;
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+            return $item;
+        }
+
+        $item->quality +=  1;
+
+        if ($item->sellIn < 10) $item->quality += 1;
+        if ($item->sellIn < 5) $item->quality += 1;
+
+        return $item;
     }
 }
